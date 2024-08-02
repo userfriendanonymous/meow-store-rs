6ac74109ee7ca9d7a0624a6b5f79e3f3a4762c7f
UserFriend <userfriendanonymous@protonmail.com>
2024-08-02 16:32:23 +0500

    shuttle

--- db_http_server/src/lib.rs
+++ db_http_server/src/lib.rs
@@ -1,9 +1,11 @@
 
 
 use std::borrow::Cow;
+use std::convert::Infallible;
 use std::path::Path;
 use std::sync::Arc;
 use std::{rc::Rc, str::FromStr};
+pub use db::OpenMode;
 use tokio::sync::{mpsc, RwLock};
 use warp::Filter;
 use warp::filters::path::param as warp_param;
@@ -15,8 +17,8 @@ mod users;
 
 // meilisearch --master-key="aSampleMasterKey"
 
-pub fn router(db: Arc<RwLock<db::Value>>)
--> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone + Send + Sync + 'static {
+fn router(db: Arc<RwLock<db::Value>>)
+-> impl Filter<Extract = (impl warp::Reply,), Error = Infallible> + Clone + Send + Sync + Sized + 'static {
     warp::path("users").and(
         users::filter(db.clone())
     )
@@ -26,75 +28,35 @@ pub fn router(db: Arc<RwLock<db::Value>>)
     )
 }
 
-pub async fn create_ctx() -> Arc<RwLock<db::Value>> {
-    let meili_client = MeiliClient::new("http://localhost:7700", Some("aSampleMasterKey")).unwrap();
-
-    let (error_sender, mut error_receiver) = mpsc::channel(20);
-
-    let error_handle = tokio::spawn(async move {
-        while let Some(err) = error_receiver.recv().await {
-            println!("[INTERNAL ERROR]: {err:?}");
-        }
-    });
-
-    let mut db = unsafe {
-        db::Value::open(
-            meili_client,
-            Path::new("database_storage"),
-            db::OpenMode::Existing,
-            error_sender,
-        ).unwrap()
-    };
-
-    // db.add_user(db::user::Value {
-    //     name: "griffpatch".parse().unwrap(),
-    //     id: 104492,
-    //     scratch_team: false,
-    //     status: Cow::Borrowed("Some status..."),
-    //     bio: Cow::Borrowed("Some cool bio!"),
-    // }).await.unwrap();
-
-    let db = Arc::new(RwLock::new(db));
-    db
-}
-
-async fn r() {
+pub async fn main(mode: OpenMode)
+-> impl Filter<Extract = (impl warp::Reply,), Error = Infallible> + Clone + Send + Sync + Sized + 'static
+{
     std::env::set_var("RUST_BACKTRACE", "1");
 
     let meili_client = MeiliClient::new("http://localhost:7700", Some("aSampleMasterKey")).unwrap();
 
     let (error_sender, mut error_receiver) = mpsc::channel(20);
-
-    let error_handle = tokio::spawn(async move {
+    let _error_handle = tokio::spawn(async move {
         while let Some(err) = error_receiver.recv().await {
             println!("[INTERNAL ERROR]: {err:?}");
         }
     });
 
+    let dir_path = Path::new("database_storage");
+    if let OpenMode::New = mode {
+        tokio::fs::create_dir_all(dir_path).await.unwrap();
+    }
+
     let mut db = unsafe {
         db::Value::open(
             meili_client,
-            Path::new("database_storage"),
-            db::OpenMode::Existing,
+            dir_path,
+            mode,
             error_sender,
         ).unwrap()
     };
-
-    // db.add_user(db::user::Value {
-    //     name: "griffpatch".parse().unwrap(),
-    //     id: 104492,
-    //     scratch_team: false,
-    //     status: Cow::Borrowed("Some status..."),
-    //     bio: Cow::Borrowed("Some cool bio!"),
-    // }).await.unwrap();
-
     let db = Arc::new(RwLock::new(db));
 
     let filter = router(db.clone());
-        
-    warp::serve(filter)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
-
-    error_handle.await.unwrap();
+    filter
 }
--- db_shuttle/src/main.rs
+++ db_shuttle/src/main.rs
@@ -1,9 +1,9 @@
+use db_http_server::OpenMode;
 use warp::Filter;
 use warp::Reply;
 
 #[shuttle_runtime::main]
 async fn warp() -> shuttle_warp::ShuttleWarp<(impl Reply,)> {
-    let db = db_http_server::create_ctx().await;
-    let route = db_http_server::router(db);
+    let route = db_http_server::main(OpenMode::New).await;
     Ok(route.boxed().into())
 }
